6041348314dcc5fd6883bf6714453988300f4ebd
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2017-07-03 13:13:20 -0400

    Code to get all users

---
+++ src/Utils/GetAllUsers.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export default () => {
+    const PROMISE = new Promise (function (resolve, reject) {
+        axios({
+            method: 'get',
+            url: 'https://demo.flyve.org/glpi/apirest.php/User/'
+        })
+            .then((response) => {
+                resolve (response.data)
+            })
+            .catch((error) => {
+                resolve (undefined)
+            })
+    })
+    return PROMISE
+}
