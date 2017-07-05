168ca1affbb0035a16b462e8287478f748eaee02
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2017-07-05 09:33:56 -0400

    Change 'HashRouter' for 'BrowserRouter'

--- src/Utils/GetCredentials.js
+++ src/Utils/GetCredentials.js
@@ -1,12 +1,61 @@
+import axios from 'axios'
+
 export default (id, password, history) => {
     navigator.credentials.get({
         password: true,
-        unmediated: true
+        unmediated: false
     })
         .then(function(cred) {
             if (cred) {
                 console.log(cred)
-                // Use provided credential to sign user in  
+
+                // var req = new Request(request.url, {
+                //     method: request.method,
+                //     headers: request.headers,
+                //     mode: 'same-origin', // need to set this properly
+                //     credentials: request.credentials,
+                //     redirect: 'manual'   // let browser handle redirects
+                // })
+
+
+
+                // fetch('http://n1jolfx3b3pb.runscope.net', {          
+                //     method: 'POST',      
+                //     credentials: cred    
+                // }).then(function(response) {
+                //     console.log(response)
+                // })
+
+
+
+
+                // axios({
+                //     method: 'post',
+                //     url: 'http://n1jolfx3b3pb.runscope.net',
+                //     auth: {
+                //         username: cred.login,
+                //         password: cred.password
+                //     }
+                // })
+                //     .then((response) => {
+                //         console.log(response)
+                //     })
+                //     .then((error) => {
+                //         console.log(error.response)
+                //     })
+
+
+
+
+
+                // axios.post ('https://dev.flyve.org/glpi/apirest.php/initSession', cred)  
+                //     .then((response) => {
+                //         console.log(response)
+                        
+                //         this.props.changeLoading('')
+                //         ChangeSessionToken(response.data.session_token)
+                //         // VerifyAccountActivation(this.props.history, 'users')
+                //     })
             }
         })
     return null
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import Login from './Login'
 import Users from './Users'
 import ValidateAccount from './Login/ValidateAccount'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import './Colors.css'
 
@@ -17,14 +17,14 @@ const store = createStore(Reducers)
 
 ReactDOM.render(
   <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Switch>
           <Route exact={true} path="/" component={App} />
           <Route path="/login" component={Login} />
           <Route path="/users" component={Users} />
           <Route path="/validateaccount" component={ValidateAccount} />
         </Switch>
-      </HashRouter >
+      </BrowserRouter >
     </Provider>,
   document.getElementById('app') as HTMLElement
 )
