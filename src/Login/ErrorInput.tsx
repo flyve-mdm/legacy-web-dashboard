661a65ff66663d3421f474eeba6170bb77df1638
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2017-07-05 15:40:36 -0400

    Add email validation with regex in javascript

--- src/Login/ErrorInput.tsx
+++ src/Login/ErrorInput.tsx
@@ -2,11 +2,16 @@ import * as React from 'react'
 
 export default class ErrorInput extends React.Component<any, any> {
     render() {
+        let message = <span />
         if (this.props.value === '' || this.props.value === undefined || this.props.value === null) {
-            return <label className="color-type-alert">Required field</label>
-        } else {
-            return <span />
+            message = <label className="color-type-alert">Required field</label>
+        } else if (this.props.name === 'userName') {
+            // tslint:disable-next-line:max-line-length
+            let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            if (!re.test(this.props.email)) {
+                message = <label className="color-type-alert">Required field</label>
+            } 
         }
-        
+        return message
     }
 }
