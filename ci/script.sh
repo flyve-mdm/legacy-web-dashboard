925762e2963dc0b8d152b77b064ef95a2b6365bc
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-13 16:08:17 -0400

    ci(screenshots): add puppeteer of GoogleChrome

--- ci/script.sh
+++ ci/script.sh
@@ -1,18 +1,20 @@
 if [[ -n $GH_TOKEN ]]; then
     git config --global user.email $GH_EMAIL
     git config --global user.name $GH_NAME
-    git config --list
     git remote remove origin
-    git remote -v
     git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git 
 fi
 
-if [[ "$TRAVIS_BRANCH" == "feature/screenshots" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    google-chrome-stable --headless --disable-gpu --screenshot https://flyve-mdm.github.io/flyve-mdm-web-dashboard/
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    npm i puppeteer
+    node ci/screenshots.js
+    git add screenshots
+    # NOTICE: this commit is not sent
+    git commit -m "temp"
     git fetch origin gh-pages
     git checkout gh-pages
-    git checkout develop screenshot.png
-    git add screenshot.png
+    git checkout $TRAVIS_BRANCH screenshots
+    git add screenshots
     git commit -m "docs(screenshots): add screenshots of the project"
     
     git push origin gh-pages
