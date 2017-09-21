13c862bf94321690701d0570caab82593b8a8158
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-21 12:18:51 -0400

    ci(screenshots): delete unnecessary folder

--- ci/script.sh
+++ ci/script.sh
@@ -3,17 +3,19 @@ if [[ -n $GH_TOKEN ]]; then
     git config --global user.name $GH_NAME
     git remote remove origin
     git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git 
+    git checkout $TRAVIS_BRANCH -f
 fi
 
-if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+if [[ "$TRAVIS_BRANCH" == "feature/screenshots" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     npm i puppeteer
+    mkdir "screenshots"
     node ci/screenshots.js
-    git add screenshots
+    git add .
     # NOTICE: this commit is not sent
     git commit -m "temp"
     git fetch origin gh-pages
     git checkout gh-pages
-    git checkout $TRAVIS_BRANCH screenshots
+    git checkout $TRAVIS_BRANCH screenshots/
     git add screenshots
     git commit -m "docs(screenshots): add screenshots of the project"
     
