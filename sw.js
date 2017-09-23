b44738d33d87934747f8957297ed48580c1293d5
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-22 20:35:13 -0400

    ci(travis): add test

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
 self.addEventListener('install', function(e) { 
   
   {% assign name = site.github.project_title | replace: "flyve-mdm-", "" %} 
-  
-  var CACHE_NAME = '{{name}}-version-1'
+
+  var CACHE_NAME = '{{name}}-version-5'
 
   caches.keys().then(function(cacheNames) {
     return Promise.all(
@@ -16,28 +16,30 @@ self.addEventListener('install', function(e) {
       })
     )
   })
-  
+
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
         '{{ site.baseurl }}/',
         '{{ site.baseurl }}/?homescreen=1',
-        '{{ "/index.html" | relative_url }}',
-        '{{ "/index.html" | relative_url }}?homescreen=1',
-        '{{ "/css/flyve-mdm.min.css" | relative_url }}',
-        '{{ "/css/main.css" | relative_url }}',
-        '{{ "css/syntax.css" | relative_url }}',
-        '{{ "images/shema01.png" | relative_url }}',
-        '{{ "images/typo.png" | relative_url }}',
-        '{{ "images/logo.png" | relative_url }}',
-        '{{ "js/app.js" | relative_url }}',
-        '{{ "js/jquery.min.js" | relative_url }}',
-        '{{ "js/bootstrap.min.js" | relative_url }}',
-        '{{ "images/logo.png" | relative_url }}',
-        '{{ "manifest.json" | relative_url }}',
-        '{{ "fonts/glyphs/winjs-symbols.ttf" | relative_url }}',
-        '{{ "images/logo.png" | relative_url }}',
-        '{{ "fonts/selawk.ttf" | relative_url }}',
+        '{{ "/index.html" | absolute_url }}',
+        '{{ "/index.html" | absolute_url }}?homescreen=1',
+        '{{ "/css/flyve-mdm.min.css" | absolute_url }}',
+        '{{ "/css/main.css" | absolute_url }}',
+        '{{ "css/syntax.css" | absolute_url }}',
+        '{{ "images/lenovo.jpg" | absolute_url }}',
+        '{{ "images/xperia.jpg" | absolute_url }}',
+        '{{ "images/blackBerry.jpg" | absolute_url }}',
+        '{{ "images/typo.png" | absolute_url }}',
+        '{{ "images/logo.png" | absolute_url }}',
+        '{{ "js/app.js" | absolute_url }}',
+        '{{ "js/jquery.min.js" | absolute_url }}',
+        '{{ "js/bootstrap.min.js" | absolute_url }}',
+        '{{ "images/logo.png" | absolute_url }}',
+        '{{ "manifest.json" | absolute_url }}',
+        '{{ "fonts/glyphs/winjs-symbols.ttf" | absolute_url }}',
+        '{{ "images/logo.png" | absolute_url }}',
+        '{{ "fonts/selawk.ttf" | absolute_url }}',
       ])
     })
   )
